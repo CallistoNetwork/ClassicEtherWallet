dd0ef3da5c6c10236cb9948404192393c39419f7
Akira Takizawa <akx20000@protonmail.com>
2018-03-25 01:52:44 +0900

    Add Ethereum Social chain support

--- app/scripts/nodes.js
+++ app/scripts/nodes.js
@@ -21,6 +21,7 @@ nodes.nodeTypes = {
     POA: "POA",
     TOMO: "TOMO",
     ELLA: "ELLA",
+    ETSC: "ETSC",
     Custom: "CUSTOM ETH"
 };
 nodes.tldTypes = {
@@ -80,6 +81,11 @@ nodes.alternativeBalance = {
         balance: "Loading",
         node: "exp",
         symbol: "EXP"
+    },
+    ETSC: {
+        balance: "Loading",
+        node: "etsc",
+        symbol: "ETSC"
     }
 };
 
@@ -354,6 +360,19 @@ nodes.nodeList = {
         'estimateGas': true,
         'service': 'ellaism.org',
         'lib': new nodes.customNode('https://jsonrpc.ellaism.org', '')
+    },
+    'etsc': {
+        'name': 'ETSC',
+        'blockExplorerTX': 'https://explorer.ethereumsocial.kr/tx/[[txHash]]',
+        'blockExplorerAddr': 'https://explorer.ethereumsocial.kr/addr/[[address]]',
+        'type': nodes.nodeTypes.ETSC,
+        'eip155': true,
+        'chainId': 214,
+        'tokenList': require('./tokens/etscTokens.json'),
+        'abiList': require('./abiDefinitions/etscAbi.json'),
+        'estimateGas': true,
+        'service': 'ethereumsocial.kr',
+        'lib': new nodes.customNode('https://node.ethereumsocial.kr', '')
     }
 };
 

3a66ca41af84a05c432545be488d0b9d75d1f317
joe <joe@joes-MacBook-Pro.local>
2018-03-29 11:36:24 -0400

    fix below 0 value array input

--- app/scripts/directives/arrayInputDrtv.js
+++ app/scripts/directives/arrayInputDrtv.js
@@ -32,21 +32,72 @@ var arrayInputDrtv = function () {
                     </div>
                         <div class="row">
                             <div class="col-xs-10">
+                            
+                            <!-- 
+                            
+                            having trouble updating displayInput 
+                                 watch is not firing
+                            
+                            --> 
+                            
+                                <!--<div ng-switch on="type_">-->
+                                    <!--<div class="item write-address" ng-switch-when="address">-->
+                                    <!--<label> {{input.name}} <small> {{type_}} </small> </label>-->
+                                    <!--<div class="row">-->
+                                      <!--<div class="col-xs-11"><input class="form-control" type="text" placeholder="0x314156..."-->
+                                                                    <!--ng-model="displayInput"-->
+                                                                    <!--ng-change="show($event)"-->
+                                                                    <!--ng-class="Validator.isValidAddress(displayInput) ? 'is-valid' : 'is-invalid'"/></div>-->
+                                      <!--<div class="col-xs-1"><div class="addressIdenticon med" title="Address Indenticon"-->
+                                                                 <!--blockie-address="{{displayInput}}" watch-var="displayInput"></div></div>-->
+                                    <!--</div>-->
+                                    <!--</div>-->
+                                    <!--<p class="item write-unit256" ng-switch-when="uint256">-->
+                                      <!--<label> {{input.name}} <small> {{type_}} </small> </label>-->
+                                      <!--<input class="form-control" type="text" placeholder="151" ng-model="displayInput"-->
+                                             <!--ng-class="Validator.isPositiveNumber(displayInput) ? 'is-valid' : 'is-invalid'"/>-->
+                                    <!--</p>-->
+                                    <!--<p class="item write-string" ng-switch-when="string">-->
+                                      <!--<label> {{input.name}} <small> {{type_}} </small> </label>-->
+                                      <!--<input class="form-control" type="text" placeholder="Ohh! Shiny!" ng-model="displayInput"-->
+                                             <!--ng-class="displayInput!='' ? 'is-valid' : 'is-invalid'"/>-->
+                                    <!--</p>-->
+                                    <!--<p class="item write-bytes" ng-switch-when="bytes">-->
+                                      <!--<label> {{input.name}} <small> {{type_}} </small> </label>-->
+                                      <!--<input class="form-control" type="text" placeholder="0x151bc..." ng-model="displayInput"-->
+                                             <!--ng-class="Validator.isValidHex(displayInput) ? 'is-valid' : 'is-invalid'"/>-->
+                                    <!--</p>-->
+                                    <!--<p class="item write-boolean" ng-switch-when="bool">-->
+                                      <!--<label> {{input.name}} <small> {{type_}} </small> </label>-->
+                                      <!--<span class="radio"><label><input ng-model="displayInput" type="radio" name="optradio-{{input.name}}"-->
+                                                                        <!--ng-value="true">True</label></span>-->
+                                      <!--<span class="radio"><label><input ng-model="displayInput" type="radio" name="optradio-{{input.name}}"-->
+                                                                        <!--ng-value="false">False</label></span>-->
+                                    <!--</p>-->
+                                    <!--<p class="item" ng-switch-default>-->
+                                      <!--<label> {{input.name}} <small> {{type_}} </small> </label>-->
+                                      <!--<input class="form-control" type="text" placeholder="" ng-model="displayInput"-->
+                                             <!--ng-class="displayInput!='' ? 'is-valid' : 'is-invalid'"/>-->
+                                    <!--</p>-->
+                                <!--</div>-->
+                                                    
                                 <input type="text" class="form-control" ng-model="displayInput" />
                             </div>
                             <div class="col-xs-2 text-center">
-                                <input type="number" class="form-control" min="0" step="1" ng-model="activeInput"/>
+                                <input type="number" class="form-control" min="{{minimum}}" step="1" ng-model="activeInput"/>
                             </div>
                         </div>
                           <div class="row">
-                              <div ng-repeat="(idx, input) in inputs track by $index">
+                              <div ng-repeat="(idx, input_) in inputs track by $index">
                     
                                   <div class="row">
                                       <div class="col-xs-2">
                                          ↳ [ {{idx}} ]
                                       </div>
                                       <div class="col-xs-8">
-                                          <input type="text" class="form-control" ng-model="input.text" readonly tabindex="-1" />
+                                      
+                                        
+                                          <input type="text" class="form-control" ng-model="input_.text" readonly tabindex="-1" />
                                       </div>
                                       <div class="col-xs-2" >
                                         <button class="btn btn-danger" ng-click="deleteInput(idx)" translate="DELETE">
@@ -65,11 +116,20 @@ var arrayInputDrtv = function () {
 
             const {type} = attr;
 
+            const type_ = type.replace('[]', '');
+
+            $scope.type = type;
+
+            $scope.type_ = type_;
+
             $scope.inputs = [
                 {
-                    text: defaultVal(type),
+                    text: defaultVal(type_),
                 }
             ];
+
+            $scope.minimum = 0;
+
 
             $scope.activeInput = 0;
 
@@ -79,7 +139,7 @@ var arrayInputDrtv = function () {
 
                 while ($scope.inputs.length <= value) {
 
-                    $scope.inputs.push({text: defaultVal(type)});
+                    $scope.inputs.push({text: defaultVal(type_)});
 
                     value--;
 
@@ -94,6 +154,22 @@ var arrayInputDrtv = function () {
                 if ($scope.inputs.length <= $scope.activeInput) {
 
                     $scope.activeInput = $scope.inputs.length - 1;
+                }
+
+                $scope.activeInput = Math.max($scope.inputs.length - 1, 0);
+
+
+                if (!$scope.inputs.length) {
+
+                    $scope.inputs = [
+                        {
+                            text: defaultVal(type_),
+                        }
+                    ];
+
+                    $scope.displayInput = $scope.inputs[$scope.activeInput].text;
+
+
                 }
 
             };

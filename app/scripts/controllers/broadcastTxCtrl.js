9c84a887eb9daaaeac55a191d7ef770d37a01b46
joe <joe@joes-mbp.home>
2018-06-01 08:12:06 -0700

    clean

--- app/scripts/controllers/broadcastTxCtrl.js
+++ app/scripts/controllers/broadcastTxCtrl.js
@@ -53,31 +53,6 @@ var broadcastTxCtrl = function ($scope) {
         });
 
 
-        /*
-
-        uiFuncs.sendTx($scope.signedTx, function (resp) {
-            if (!resp.isError) {
-                var txHashLink = ajaxReq.blockExplorerTX.replace("[[txHash]]", resp.data);
-                var verifyTxBtn = $scope.ajaxReq.type !== nodes.nodeTypes.Custom ? '<a class="btn btn-xs btn-info strong" href="' + txHashLink + '" target="_blank" rel="noopener noreferrer">Verify Transaction</a>' : '';
-                var completeMsg = '<p>' + globalFuncs.successMsgs[2] + '<strong>' + resp.data + '</strong></p>' + verifyTxBtn;
-                $scope.notifier.success(completeMsg, 0);
-                $scope.wallet.setBalance(applyScope);
-                if ($scope.tx.sendMode === 'token') $scope.wallet.tokenObjs[$scope.tokenTx.id].setBalance();
-            } else {
-
-
-                if (resp.error.includes('insufficient funds')) {
-
-                    $scope.notifier.danger(globalFuncs.errorMsgs[17].replace('{}', ajaxReq.type));
-                }
-                else {
-
-                    $scope.notifier.danger(resp.error || globalFuncs.errorMsgs[17].replace('{}', ajaxReq.type));
-                }
-
-            }
-        });
-         */
     }
 
 
@@ -117,9 +92,6 @@ var broadcastTxCtrl = function ($scope) {
             value: ethUtil.solidityCoder.decodeParam('int256', $scope.input.rawTx.value).toNumber(),
         });
 
-        console.log(tx);
-
-        console.log($scope.input.rawTx);
 
     }
 

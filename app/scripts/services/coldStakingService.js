801fe939c3acb6c533abe5ff57cf740b7e2005bb
joe <joe@joes-MacBook-Pro.local>
2018-06-14 22:14:45 -0700

    service

--- app/scripts/services/coldStakingService.js
+++ app/scripts/services/coldStakingService.js
@@ -2,7 +2,7 @@ var coldStakingService = function (walletService) {
 
 
         this.contractAddrs = {
-            CLOT: '0xa45083107ae67636cd9b93ad13c15b939dbdce31',
+            'CLO Testnet 3.0': '0xa45083107ae67636cd9b93ad13c15b939dbdce31',
             // fixme: testing addr
             'RINKEBY ETH': '0x713f80e73b174b9aba62dd75fa1da6925c13ace5',//'0xa3a278371d1569d849f93f4241c7812969e863a3',
             // CLO: '0x',

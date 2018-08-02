d7db8beb94c8701a85cb432e6304f794686808c6
joe <jchimien@gmail.com>
2018-08-02 10:14:57 -0400

    add copy addr to clipboard component

--- app/scripts/directives/balanceDrtv.js
+++ app/scripts/directives/balanceDrtv.js
@@ -1,10 +1,48 @@
 'use strict';
 
-var balanceDrtv = function() {
-	return {
-        restrict : "E",
-        template : require('./balanceDrtv.html'),
+var balanceDrtv = function (walletService, $timeout) {
+    return {
+        restrict: "E",
+        template: require('./balanceDrtv.html'),
+        link: function (scope) {
 
-  };
+            scope.displayTime = 0;
+
+            function displaySuccess() {
+
+                scope.displayTime = 1;
+
+                $timeout(function () {
+
+                    scope.displayTime = 0;
+                }, 3000)
+
+            }
+
+            scope.copyToClipboard = function copyToClipboard(elementId = 'addr') {
+
+                // Create a "hidden" input
+                var aux = document.createElement("input");
+                // Assign it the value of the specified element
+                aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+                // Append it to the body
+                document.body.appendChild(aux);
+                // Highlight its content
+                aux.select();
+                // Copy the highlighted text
+                const result = document.execCommand("copy");
+                // Remove it from the body
+                document.body.removeChild(aux);
+
+                if (result) {
+
+
+                    displaySuccess();
+                }
+
+            }
+        }
+
+    };
 };
 module.exports = balanceDrtv;

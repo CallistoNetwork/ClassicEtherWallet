cd92c0567f951594428fcbe8a6fb18fa9d99459b
joe <jchimien@gmail.com>
2018-09-11 10:11:08 -0400

    validate tx.gasLimit * tx.gas + value <= wallet.balance
    changes:
    
    tx value & gasLimit type = number
    validation min / max value
    validation min gasLimit
    
    notify user insufficient balance

--- app/scripts/directives/sendTransactionForm.js
+++ app/scripts/directives/sendTransactionForm.js
@@ -1,11 +1,16 @@
 "use strict";
 
-module.exports = function sendTransactionForm() {
+const BigNumber = require("bignumber.js");
+const etherUnits = require("../etherUnits");
+
+module.exports = function sendTransactionForm(walletService) {
     return {
         template: require("./sendTransactionForm.html"),
         restrict: "A",
         require: "form",
         link: function(scope, element, attrs, form) {
+            Object.assign(scope.tx, { totalTxCost: 0 });
+
             // address field passed into lookup service (async function)
 
             function validForm() {
@@ -19,6 +24,22 @@ module.exports = function sendTransactionForm() {
             };
             form.gasLimit.$validators.gasLimit = form.value.$validators.value =
                 Validator.isPositiveNumber;
+
+            form.value.$validators.enoughBalance = _val => {
+                if (!_val) return true;
+                const txCost = new BigNumber(
+                    etherUnits.toWei(_val, "ether")
+                ).add(etherUnits.toWei(scope.txCostEther, "ether"));
+                const _balance = new BigNumber(
+                    etherUnits.toWei(walletService.wallet.balance, "ether")
+                );
+
+                Object.assign(scope.tx, {
+                    totalTxCost: txCost.toString()
+                });
+
+                return txCost.lt(_balance);
+            };
         }
     };
 };

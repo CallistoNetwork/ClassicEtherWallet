15d8ec28b093ef6e5507939645133e4cc4d3dbd2
joe <jchimien@gmail.com>
2018-09-15 09:02:34 -0400

    add error displays

--- app/scripts/directives/generateWalletForm.js
+++ app/scripts/directives/generateWalletForm.js
@@ -1,15 +1,16 @@
 "use strict";
 
-module.exports = function() {
+module.exports = function generateWalletForm() {
     return {
         template: require("./generateWalletForm.html"),
         restrict: "A",
         require: "form",
         link: function(scope, e, attr, form) {
-            form.password.$validators.validPassword = globalFuncs.isStrongPass;
+            form.password.$validators.validPassword = form.password2.$validators.validPassword =
+                globalFuncs.isStrongPass;
 
             form.password2.$validators.matchesPassword = _val =>
-                angular.equals(scope.userInupt.password, _val);
+                angular.equals(scope.userInput.password, _val);
         }
     };
 };
